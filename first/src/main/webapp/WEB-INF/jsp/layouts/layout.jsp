8819597c35b060d3f7a1175f1d6275716d5cfbab
wise <wise@wise-hp>
2017-07-11 14:42:05 +0900

    부트 앤 타일즈 적용

---
+++ first/src/main/webapp/WEB-INF/jsp/layouts/layout.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+
+<html>
+<head>
+<title>Home</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<c:set var="contextPath" value="<%= request.getContextPath()%>"></c:set>  
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="${contextPath}/resources/bootstrap/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="${contextPath}/resources/js/jquery-3.2.1.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="${contextPath}/resources/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- 공통 스크립트 -->
+<script src="${contextPath}/resources/js/common.js"></script>
+
+
+</head>
+<body>
+	<div class="container-fluid">
+		<tiles:insertAttribute name="header" />
+	</div>
+	<div class="container">
+		<tiles:insertAttribute name="main" />
+	</div>
+	<div class="container-fluid">
+		<tiles:insertAttribute name="footer" />
+	</div>
+</body>
+</html>
---
+++ first/src/main/webapp/resources/js/common.js
@@ -0,0 +1,52 @@
+// ================================================
+// ajax 처리 후 콜백함수...
+// ================================================
+function ajaxCall(url, param, callback) {
+		 $.ajax({           
+	   url: url,                 
+	   async: false,                    
+	   type: "POST",           
+	   data: param,                                    
+	   dataType: 'json', 
+	   beforeSend : function(xhr) {// ajax 호출시 사용자 인증정보가 잇는지 확인
+				xhr.setRequestHeader("AJAX", "true");                  
+			},
+	   success: callback,                             
+	   error:function(request,textStatus){
+	  	 if(request.status == 9999){                
+	  		 $("form").eq(0).attr("action",getContextPath+"/main.do").submit();
+	  	 }else{
+	  		alert("다시 시도하여 주시기 바랍니다.");
+	  		 //alert('에러발생...' + textStatus);
+	  	 }
+	   }
+	});
+}
+
+
+/* 팝업으로 열기 */
+function OpenWindow(target, wName, wWidth, wHeight, wScroll){
+	
+  var xpos = ( screen.width -  wWidth  ) / 2; 
+  var ypos = ( screen.height - wHeight )  / 2;  
+  
+  //윈도우 이름이 틀리면 새창으로 팝업이 열리고, 같으면 이름이 같이 창이 Replace된다.
+  if (wName == null) wName = "";
+	  
+  popupWindow = window.open(target,wName,'toolbar=no,status=no,top='+ ypos +',left='+ xpos +',width=' + wWidth + ',height=' + wHeight +',directories=no,scrollbars=yes,location=no,resizable=yes,border=0,menubar=no,status=yes');
+  popupWindow.focus();
+  return popupWindow;
+}
+
+/* 모달 팝업으로 열기 */
+function OpenModal(target, wName, wWidth, wHeight, wScroll){
+	
+	var xpos = ( screen.width -  wWidth  ) / 2; 
+	var ypos = ( screen.height - wHeight )  / 2;  
+	
+	//윈도우 이름이 틀리면 새창으로 팝업이 열리고, 같으면 이름이 같이 창이 Replace된다.
+	if (wName == null) wName = "";
+	
+	popupWindow = window.open(target,wName,'top='+ ypos +',left='+ xpos +',width=' + wWidth + ',height=' + wHeight +',directories=no,scrollbars=yes,location=no,resizable=yes,border=0,menubar=no,status=no,toolbar=no,modal=yes');
+	return popupWindow;
+}
